d84695944b12dc3f2e9a3b12a3d8bd7e6bf90b2e
Hammad Kakli <hammadkakli@gmail.com>
2023-03-29 11:15:54 +0000

    update contact form

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -185,8 +185,7 @@ const ContactForm: FC = (): ReactElement => {
               >
                 Phone:
               </h3>
-              <p className="paragraph">(+92)
-                300 8089934</p>
+              <p className="paragraph">(+92) 300 8089934</p>
             </div>
           </div>
         </div>
